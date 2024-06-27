ae1f0f3316f665dc4d423eccda6c1d41cd72cc40
Sifat Mahmud <sifatmahmud5293@gmail.com>
2024-06-27 15:40:57 +0600

    Module-07 for singly linked list recap

---
+++ Data structure/Module-07/delete_head.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// ======== insert at tail function =========
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+// ============ delete head function =============
+void delete_head(Node *&head)
+{
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+// ============ delete node from list function =============
+void delete_node(Node *head, int pos)
+{
+    Node *temp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        temp = temp->next;
+    }
+    Node *deleteNode = temp->next;
+    temp->next = temp->next->next;
+    delete deleteNode;
+}
+// ============ size of list function =============
+int size_list(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+// ============ print list function =============
+void print_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// ============ main function ==============
+int main()
+{
+    Node *head = NULL; // initialization
+    Node *tail = NULL;
+    while (true) // input linked list
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    int pos;
+    cin >> pos;
+    if (pos >= size_list(head))
+    {
+        cout << "Invalid !!" << endl;
+    }
+    else if (pos == 0)
+    {
+        delete_head(head);
+    }
+    else
+    {
+        delete_node(head, pos);
+        print_list(head);
+    }
+
+    return 0;
+}
---
+++ Data structure/Module-07/singly_linked_list.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// ========= function area =============
+void insert_index()
+{
+}
+int main()
+{
+
+    return 0;
+}
